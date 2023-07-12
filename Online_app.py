6dda2f2bd17993cf284d0981dce09e0bfb54380d
hszym <45387296+hszym@users.noreply.github.com>
2023-07-12 14:44:30 +0200

    Update Online_app.py

--- Online_app.py
+++ Online_app.py
@@ -7,6 +7,18 @@ import seaborn as sns
 # Set Seaborn style
 sns.set_style('darkgrid')
 
+def get_stock_description(stock_name):
+    # Retrieve stock information using yfinance
+    stock = yf.Ticker(stock_name)
+    info = stock.info
+
+    # Extract relevant information
+    description = info.get('longBusinessSummary', 'N/A')
+    sector = info.get('sector', 'N/A')
+    industry = info.get('industry', 'N/A')
+
+    return description, sector, industry
+
 def plot_stocks_with_barrier(stock_names, barrier_level, period):
     # Create a DataFrame to store the stock data
     stock_data = pd.DataFrame()
@@ -25,7 +37,7 @@ def plot_stocks_with_barrier(stock_names, barrier_level, period):
     plt.axhline(barrier_level, color='red', linestyle='--', label='Barrier Level', linewidth=2)
     plt.xlabel('Date')
     plt.ylabel('Normalized Price (Base 100 from the end)')
-    plt.title('Stock Prices with Barrier Level')
+    plt.title('Optimize your idea')
     plt.legend()
     plt.tight_layout()
 
@@ -38,7 +50,7 @@ def plot_stocks_with_barrier(stock_names, barrier_level, period):
 # Streamlit app code
 def main():
     # Set the page title
-    st.title('Optmize your product')
+    st.title('Stock Chart with Barrier Level')
 
     # Input stock names and barrier level
     stock_names = st.text_input('Enter Stock Names (comma-separated)', 'AAPL,GOOGL,MSFT').split(',')
@@ -48,6 +60,20 @@ def main():
     # Check if the barrier level is within the valid range
     if 0 <= barrier_level <= 200:
         plot_stocks_with_barrier(stock_names, barrier_level, period)
+
+        # Display stock descriptions and summary
+        st.subheader('Stock Descriptions')
+        for stock_name in stock_names:
+            st.write(f"**{stock_name}**")
+            description, sector, industry = get_stock_description(stock_name)
+            st.write(f"Description: {description}")
+            st.write(f"Sector: {sector}")
+            st.write(f"Industry: {industry}")
+            st.write('---')
+
+        st.subheader('Investment Summary')
+        st.write('Based on historical data and market trends, investing in these stocks could be interesting due to various factors such as strong financial performance, growth potential, and industry dominance. However, it is important to conduct thorough research and consider your own investment goals and risk tolerance before making any investment decisions.')
+
     else:
         st.error('Invalid barrier level. Please enter a value between 0 and 200.')
 
