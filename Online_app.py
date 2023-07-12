a0ab51819c5527f9f42180f259dbe1cae41be055
hszym <45387296+hszym@users.noreply.github.com>
2023-07-12 14:35:02 +0200

    Update Online_app.py

--- Online_app.py
+++ Online_app.py
@@ -2,28 +2,35 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def plot_stocks_with_barrier(stock_names, barrier_level):
+# Set Seaborn style
+sns.set_style('darkgrid')
+
+def plot_stocks_with_barrier(stock_names, barrier_level, period):
     # Create a DataFrame to store the stock data
     stock_data = pd.DataFrame()
 
     # Fetch stock data for each stock name
     for stock_name in stock_names:
         stock = yf.Ticker(stock_name)
-        stock_history = stock.history(period='3y')['Close']
+        stock_history = stock.history(period=period)['Close']
         stock_data[stock_name] = stock_history / stock_history.iloc[-1] * 100  # Normalize to base 100 from the end
 
     # Plotting the stock prices
     plt.figure(figsize=(10, 6))
     for stock_name in stock_names:
-        plt.plot(stock_data[stock_name], label=stock_name)
-        plt.axhline(stock_data[stock_name].iloc[-1] * barrier_level, linestyle='--', label=f'{stock_name} Barrier Level')
+        plt.plot(stock_data[stock_name], label=stock_name, linewidth=2)
 
+    plt.axhline(barrier_level, color='red', linestyle='--', label='Barrier Level', linewidth=2)
     plt.xlabel('Date')
     plt.ylabel('Normalized Price (Base 100 from the end)')
     plt.title('Stock Prices with Barrier Level')
     plt.legend()
-    plt.grid(True)
+    plt.tight_layout()
+
+    # Set plot style
+    sns.despine()
 
     # Display the plot
     st.pyplot(plt)
@@ -35,13 +42,14 @@ def main():
 
     # Input stock names and barrier level
     stock_names = st.text_input('Enter Stock Names (comma-separated)', 'AAPL,GOOGL,MSFT').split(',')
-    barrier_level = st.slider('Enter Barrier Level (between 0 and 100%)', 0.0, 1.0, 0.5)
+    barrier_level = st.slider('Enter Barrier Level', 0.0, 200.0, 100.0, step=0.1)
+    period = st.selectbox('Select Period', ('6mo', '1y', '1d', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'))
 
     # Check if the barrier level is within the valid range
-    if 0 <= barrier_level <= 1:
-        plot_stocks_with_barrier(stock_names, barrier_level)
+    if 0 <= barrier_level <= 200:
+        plot_stocks_with_barrier(stock_names, barrier_level, period)
     else:
-        st.error('Invalid barrier level. Please enter a value between 0 and 100%.')
+        st.error('Invalid barrier level. Please enter a value between 0 and 200.')
 
 # Run the Streamlit app
 if __name__ == '__main__':
